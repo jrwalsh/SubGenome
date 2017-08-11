316359aa06f8874da2e9a634dd24d890e4f1486e
unknown <jrwalsh@iastate.edu>
2017-08-11 02:52:06 -0500

    Provided support for a basic GO analysis
    Cleaner data slices, parsed Citation field, but needs more cleanup, we have some unused and obsolete objects generated here.
    
    Can do a basic GO analysis on the GO terms themselves, but not yet on their assignment to genes.

--- R/addGOTerms.R
+++ R/addGOTerms.R
@@ -17,6 +17,104 @@ goDataFile <- "C:\\Users\\Jesse\\Dropbox (Personal)\\Link to Subgenome Data\\GO 
 
 ## Read in GO Annotation data for maize genes
 goAnnotations <- read_delim(goDataFile, "\t", escape_double = FALSE, trim_ws = TRUE)
+goAnnotations.raw <- read_delim(goDataFile, "\t", escape_double = FALSE, trim_ws = TRUE)
+
+## Parse out citation string
+goAnnotations.mutated <-
+  goAnnotations.raw %>%
+  separate(Citation, c("Publication", "EVCode","TimeStamp","Curator"), sep=":", extra="drop")
+
+
+## GO Analysis -> grouping into useful slices
+goAnnotations.sub1 <-
+  subgenome %>%
+  subset(subgenome == "sub1") %>%
+  distinct() %>%
+  inner_join(goAnnotations.mutated, by = c("gene2" = "MaizeCyc2.2 Accession-1")) %>%
+  select(gene2, "GO Term", EVCode) %>%
+  distinct()
+
+goAnnotations.sub2 <-
+  subgenome %>%
+  subset(subgenome == "sub2") %>%
+  distinct() %>%
+  inner_join(goAnnotations.mutated, by = c("gene2" = "MaizeCyc2.2 Accession-1")) %>%
+  select(gene2, "GO Term", EVCode) %>%
+  distinct()
+
+goAnnotations.sub1.aggr <-
+  goAnnotations.sub1 %>%
+  select(`GO Term`) %>%
+  group_by(`GO Term`) %>%
+  summarise(n=n())
+
+goAnnotations.sub2.aggr <-
+  goAnnotations.sub2 %>%
+  select(`GO Term`) %>%
+  group_by(`GO Term`) %>%
+  summarise(n=n())
+
+#
+# setdiff(goAnnotations.sub1.aggr %>% select("GO Term"), goAnnotations.sub2.aggr %>% select("GO Term")) %>% nrow()
+# setdiff(goAnnotations.sub2.aggr %>% select("GO Term"), goAnnotations.sub1.aggr %>% select("GO Term"))
+#
+# full_join(goAnnotations.sub1.aggr, goAnnotations.sub2.aggr, by="GO Term", is.na=0) %>%
+#   rename(sub1_n=n.x, sub2_n=n.y) %>%
+#   subset(is.na(sub2_n)) %>%
+#   distinct() %>%
+#   nrow()
+#
+# goAnnotations.diff <-
+#   full_join(goAnnotations.sub1.aggr, goAnnotations.sub2.aggr, by="GO Term", is.na=0) %>%
+#   rename(sub1_n=n.x, sub2_n=n.y) %>%
+#   subset(is.na(sub1_n) | is.na(sub2_n))
+# goAnnotations.diff.counts <-
+#   full_join(goAnnotations.sub1.aggr, goAnnotations.sub2.aggr, by="GO Term", is.na=0) %>%
+#   rename(sub1_n=n.x, sub2_n=n.y) %>%
+#   replace_na(list(sub1_n=0, sub2_n=0)) %>%
+#   transmute(`GO Term`=`GO Term`,diff=sub1_n-sub2_n) %>%
+#   subset(diff != 0)
+# goAnnotations.same <-
+#   full_join(goAnnotations.sub1.aggr, goAnnotations.sub2.aggr, by="GO Term") %>%
+#   rename(sub1_n=n.x, sub2_n=n.y) %>%
+#   replace_na(list(sub1_n=0, sub2_n=0)) %>%
+#   transmute(`GO Term`=`GO Term`,diff=sub1_n-sub2_n) %>%
+#   subset(diff == 0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# goAnnotations.mutated <- goAnnotations.raw
+# r <- regexpr(":EV.*?:", goAnnotations.mutated$Citation)
+# goAnnotations.mutated$EVCode <- rep(NA,length(goAnnotations.mutated$Citation))
+# goAnnotations.mutated$EVCode[r!=-1] <- regmatches(goAnnotations.mutated$Citation, r)
+# goAnnotations.mutated$EVCode <-
+#   goAnnotations.mutated$EVCode %>%
+#   gsub(pattern=":", replacement="")
+
+
+# goAnnotations.mutated <-
+#   goAnnotations.raw %>%
+#   group_by(block, org_chr1, org_chr2) %>%
+#   summarise(median_ks=median(ks, na.rm=TRUE), blockGeneCount=n()) %>%
+#   left_join(syntelogs.raw, ., by = c("block", "org_chr1", "org_chr2")) %>%
+#   mutate(gene_length1=abs(stop1-start1)) %>%
+#   mutate(gene_length2=abs(stop2-start2)) %>%
+#   mutate(chr1=as.numeric(regmatches(org_chr1, regexpr("\\d*$",org_chr1))))
+#   mutate(chr2=as.numeric(regmatches(org_chr2, regexpr("\\d*$",org_chr2))))
+
 
 ## Merge v3 homeolog gene IDs to the GO term data, each line has a unique Gene -> GO term pair, where genes and go terms can be reused
 ## This list is only genes that have homeologs and have GO annotation
@@ -28,27 +126,44 @@ goAnnotations <- read_delim(goDataFile, "\t", escape_double = FALSE, trim_ws = T
 #   select(`MaizeCyc2.2 Accession-1`, `GO Term`) %>%
 #   distinct() -> homeologs.go
 
-## A simple list of (v3) genes in subgenome 1
+# ## A simple list of genes in subgenome 1
 subgenome.sub1 <-
   subgenome %>%
   filter(subgenome == "sub1") %>%
   select(gene2) %>%
   distinct()
-
-## A simple list of (v3) genes in subgenome 2
+#
+# ## A simple list of genes in subgenome 2
 subgenome.sub2 <-
   subgenome %>%
   filter(subgenome == "sub2") %>%
   select(gene2) %>%
   distinct()
+#
+# ## Rows from go annotation file annotating a gene from sub1 or sub2
+# goAnnotations.sub1 <-
+#   goAnnotations[goAnnotations$`MaizeCyc2.2 Accession-1` %in% subgenome.sub1$gene2,] %>%
+#   select(`MaizeCyc2.2 Accession-1`, `GO Term`)
+# goAnnotations.sub2 <-
+#   goAnnotations[goAnnotations$`MaizeCyc2.2 Accession-1` %in% subgenome.sub2$gene2,] %>%
+#   select(`MaizeCyc2.2 Accession-1`, `GO Term`)
 
-## Rows from go annotation file annotating a gene from sub1 or sub2
-goAnnotations.sub1 <-
-  goAnnotations[goAnnotations$`MaizeCyc2.2 Accession-1` %in% subgenome.sub1$gene2,] %>%
-  select(`MaizeCyc2.2 Accession-1`, `GO Term`)
-goAnnotations.sub2 <-
-  goAnnotations[goAnnotations$`MaizeCyc2.2 Accession-1` %in% subgenome.sub2$gene2,] %>%
-  select(`MaizeCyc2.2 Accession-1`, `GO Term`)
+subgenome.sub1.go <-
+  subgenome %>%
+  subset(subgenome == "sub1") %>%
+  distinct() %>%
+  inner_join(goAnnotations, by = c("gene2" = "MaizeCyc2.2 Accession-1")) %>%
+  select(gene2, "GO Term") %>%
+  distinct()
+
+subgenome.sub2.go <-
+  subgenome %>%
+  subset(subgenome == "sub2") %>%
+  distinct() %>%
+  inner_join(goAnnotations, by = c("gene2" = "MaizeCyc2.2 Accession-1")) %>%
+  select(gene2, "GO Term") %>%
+  distinct()
+
 
 # write.table(goAnnotations.sub1, "goAnnotationsSub1.tab", sep="\t")
 # write.table(goAnnotations.sub2, "goAnnotationsSub2.tab", sep="\t")
