4e8bd34522241406bde9f3acb6e63a6636ef0097
Jesse Walsh <jrwalsh@iastate.edu>
2017-08-18 16:44:40 -0500

    Tied GOSlim import to main report

--- R/parseGOSlim.R
+++ R/parseGOSlim.R
@@ -1,14 +1,14 @@
 # http://www.geneontology.org/ontology/subsets/goslim_plant.obo
 
-goSlim <- read_delim("./Data/goslim_plant.tab", "\t", escape_double = FALSE, trim_ws = TRUE)
+goSlim <- read_delim("../Data/goslim_plant.tab", "\t", escape_double = FALSE, trim_ws = TRUE)
 # goSlim$GOSlimTerm <- paste("|",goSlim$GOSlimTerm,"|",sep = "")
 
 
 
-goAssignmentsIn2not1_withSlim <-
-  converted %>%
-  inner_join(goSlim, by=c("GO Term" = "GOSlimTerm")) %>%
-  filter(grepl("EV-EXP", EVCode)) %>%
-  setdiff(., goAnnotations.sub1 %>%
-              inner_join(goSlim, by=c("GO Term" = "GOSlimTerm")) %>%
-              filter(grepl("EV-EXP", EVCode)))
+# goAssignmentsIn2not1_withSlim <-
+#   converted %>%
+#   inner_join(goSlim, by=c("GO Term" = "GOSlimTerm")) %>%
+#   filter(grepl("EV-EXP", EVCode)) %>%
+#   setdiff(., goAnnotations.sub1 %>%
+#               inner_join(goSlim, by=c("GO Term" = "GOSlimTerm")) %>%
+#               filter(grepl("EV-EXP", EVCode)))
