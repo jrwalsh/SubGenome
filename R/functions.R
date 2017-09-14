ac39dd2d03c07b8ed70128d49a34c1e3674e6825
Jesse Walsh <jrwalsh@iastate.edu>
2017-09-14 14:11:33 -0500

    Minor

---
+++ R/functions.R
@@ -0,0 +1,9 @@
+####################################################################################################
+## Project:         Subgenomes project
+## Script purpose:  Create functions to be sourced and used for this project
+##
+## Input:
+## Output:
+## Date: 2017-09-14
+## Author: Jesse R. Walsh
+####################################################################################################
