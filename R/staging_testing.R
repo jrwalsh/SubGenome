f5bc780ecd5cc1e534a55de1ca4182cf0187462a
Jesse Walsh <jrwalsh@iastate.edu>
2017-10-13 16:14:17 -0500

    Examining correlations

--- R/staging_testing.R
+++ R/staging_testing.R
@@ -3,9 +3,12 @@
 
 # library(readr)
 # library(GenomicFeatures)
+library(fitdistrplus)
 library(tidyr)
 library(dplyr)
 library(ggplot2)
+select <- dplyr::select
+
 
 cmdBlast <- function(query, subject) {
   paste0("blastn -query <(", query, ") -subject <(", subject, ")")
@@ -31,3 +34,41 @@ subject <- "ATGGAGATCCTCGAATCGACCCTGTTGGGCGAGTTCATCGGCTTCATCAAGGGGAACTGGTCAGCGCA
 cmdNeedle(query, subject)
 
 # ---
+
+
+data <-
+  homeologs.pairs %>%
+  subset(Maize1 != "" & Maize2 != "") %>%
+  select(Maize1, Maize2) %>%
+  distinct() %>%
+  inner_join(maize.expression.sample.avg, by=c("Maize1"="geneID")) %>%
+  inner_join(maize.expression.sample.avg, by=c("Maize2"="geneID", "Sample"="Sample")) %>%
+  subset(!is.na(FPKM_avg.x) & !is.na(FPKM_avg.y))
+names(data)[4] <- "FPKM_maize1"
+names(data)[5] <- "FPKM_maize2"
+
+data <-
+  data %>%
+  inner_join(maize.protein.abundance.sample.avg, by=c("Maize1"="geneID", "Sample"="Sample")) %>%
+  inner_join(maize.protein.abundance.sample.avg, by=c("Maize2"="geneID", "Sample"="Sample")) %>%
+  subset(!is.na(dNSAF_avg.x) & !is.na(dNSAF_avg.y))
+names(data)[6] <- "dNSAF_maize1"
+names(data)[7] <- "dNSAF_maize2"
+
+## Fit test
+descdist(data$FPKM_maize1, discrete = FALSE)
+descdist(data$FPKM_maize2, discrete = FALSE)
+descdist(data$dNSAF_maize1, discrete = FALSE)
+descdist(data$dNSAF_maize2, discrete = FALSE)
+fit.gamma <- fitdist(data$Value_maize1, "gamma", method = "mme")
+plot(fit.gamma)
+fit.gamma$aic
+fit <- logspline(stats)
+
+## Correlation test
+cor(data$FPKM_maize1, data$FPKM_maize2, method = "pearson")
+cor(data$dNSAF_maize1, data$dNSAF_maize2, method = "pearson")
+cor(data$FPKM_maize1, data$dNSAF_maize1, method = "pearson")
+
+
+detach("package:fitdistrplus", unload=TRUE)
