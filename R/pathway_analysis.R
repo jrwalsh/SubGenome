628e909a22811ae16826f210b94d78697a650eb8
Jesse Walsh <jrwalsh@iastate.edu>
2017-11-02 16:46:55 -0500

    Trying to add pathway data to analysis

---
+++ R/pathway_analysis.R
@@ -0,0 +1,94 @@
+#--------------------------------------------------------------------------------------------------#
+# Pathways
+#--------------------------------------------------------------------------------------------------#
+## Load
+corncyc.pathways.raw <- read_delim("./Data/Pathways/CornCyc_8.0.1_Pathways.tab", "\t", escape_double = FALSE, trim_ws = TRUE)
+corncyc.reactions.raw <- read_delim("./Data/Pathways/CornCyc_8.0.1_Reactions.tab", "\t", escape_double = FALSE, trim_ws = TRUE)
+corncyc.geneids.raw <- read_delim("./Data/Pathways/CornCyc_8.0.1_GeneIDs.tab", "\t", escape_double = FALSE, trim_ws = TRUE)
+
+## Clean
+corncyc.pathways.clean <- corncyc.pathways.raw
+
+# Remove superpathways, remove pathways with no genes, unnest the reaction list
+corncyc.pathways.clean <-
+  corncyc.pathways.clean %>%
+  subset(is.na(SubPathways)) %>%
+  subset(!is.na(Reactions)) %>%
+  select(PathwayID, Reactions) %>%
+  mutate(Reactions = gsub("\"","",Reactions)) %>%
+  mutate(ReactionID = strsplit(as.character(Reactions), " // ")) %>%
+  unnest(ReactionID) %>%
+  select(PathwayID, ReactionID)
+
+corncyc.reactions.clean <- corncyc.reactions.raw
+
+# Remove superpathways, remove pathways with no genes, unnest the reaction list
+corncyc.reactions.clean <-
+  corncyc.reactions.clean %>%
+  subset(!is.na(Genes)) %>%
+  mutate(GeneID = strsplit(as.character(Genes), " // ")) %>%
+  unnest(GeneID) %>%
+  select(ReactionID, GeneID)
+
+corncyc.geneids.clean <- corncyc.geneids.raw
+
+# Collect only GRMZM and Zm names which can be in either the Accession1 or Accession2 column
+corncyc.geneids.clean$v3_id <- NA
+corncyc.geneids.clean$v4_id <- NA
+corncyc.geneids.clean$v3_id <- ifelse(startsWith(corncyc.geneids.clean$Accession1, "GRMZM"),
+                                      corncyc.geneids.clean$Accession1,
+                                      ifelse(startsWith(corncyc.geneids.clean$Accession2, "GRMZM"),
+                                             corncyc.geneids.clean$Accession2,
+                                             NA))
+corncyc.geneids.clean$v4_id <- ifelse(startsWith(corncyc.geneids.clean$Accession1, "Zm"),
+                                      corncyc.geneids.clean$Accession1,
+                                      ifelse(startsWith(corncyc.geneids.clean$Accession2, "Zm"),
+                                             corncyc.geneids.clean$Accession2,
+                                             NA))
+# Remove the transcript/protein suffix.
+# For GRMZM, it is _, T or P, 2 numbers, and sometimes a "." and another number.
+# For Zm, it is _, T or P, and 3 numbers
+corncyc.geneids.clean <-
+  corncyc.geneids.clean %>%
+  mutate(v3_id=gsub("_[PT]\\d\\d.*\\d*", "", v3_id, perl=TRUE)) %>%
+  mutate(v4_id=gsub("_[PT]\\d\\d\\d.\\d", "", v4_id, perl=TRUE)) %>%
+  select(FrameID, v4_id) %>%
+  subset(!is.na(v4_id))
+
+
+# Merge the pathway/reaction sets
+pathway.reaction.genes <-
+  corncyc.pathways.clean %>%
+  inner_join(corncyc.reactions.clean, by=c("ReactionID"="ReactionID"))
+
+## Do
+# A dataset with homeologous pairs with their frameID's (where available)
+data <-
+  homeologs.pairs %>%
+  subset(Maize1 != "" & Maize2 != "") %>%
+  select(Maize1, Maize2) %>%
+  distinct() %>%
+  left_join(corncyc.geneids.clean, by=c("Maize1"="v4_id")) %>%
+  left_join(corncyc.geneids.clean, by=c("Maize2"="v4_id")) %>%
+  rename(FrameID1=FrameID.x, FrameID2=FrameID.y)
+
+# Reactions associated with Maize1 genes
+df1 <-
+  data %>%
+  subset(!is.na(FrameID1)) %>%
+  left_join(pathway.reaction.genes, by=c("FrameID1"="GeneID")) %>%
+  subset(!is.na(ReactionID)) %>%
+  select(ReactionID) %>%
+  distinct()
+# Reactions associated with Maize2 genes
+df2 <-
+  data %>%
+  subset(!is.na(FrameID2)) %>%
+  left_join(pathway.reaction.genes, by=c("FrameID2"="GeneID")) %>%
+  subset(!is.na(ReactionID)) %>%
+  select(ReactionID) %>%
+  distinct()
+
+intersect(df1, df2)
+setdiff(df1, df2)
+setdiff(df2, df1)
--- R/staging_testing.R
+++ R/staging_testing.R
@@ -36,43 +36,15 @@ subject <- "ATGGAGATCCTCGAATCGACCCTGTTGGGCGAGTTCATCGGCTTCATCAAGGGGAACTGGTCAGCGCA
 cmdNeedle(query, subject)
 
 #--------------------------------------------------------------------------------------------------#
-# Correlation between datasets
+# Pathways
 #--------------------------------------------------------------------------------------------------#
-## Start with the paired expression data
-data <-
-  homeologs.pairs %>%
-  subset(Maize1 != "" & Maize2 != "") %>%
-  select(Maize1, Maize2) %>%
-  distinct() %>%
-  inner_join(maize.expression.sample.avg, by=c("Maize1"="geneID")) %>%
-  inner_join(maize.expression.sample.avg, by=c("Maize2"="geneID", "Sample"="Sample")) %>%
-  subset(!is.na(FPKM_avg.x) & !is.na(FPKM_avg.y))
-names(data)[4] <- "FPKM_maize1"
-names(data)[5] <- "FPKM_maize2"
 
-## Add paired abundance data
-data <-
-  data %>%
-  inner_join(maize.protein.abundance.sample.avg, by=c("Maize1"="geneID", "Sample"="Sample")) %>%
-  inner_join(maize.protein.abundance.sample.avg, by=c("Maize2"="geneID", "Sample"="Sample")) %>%
-  subset(!is.na(dNSAF_avg.x) & !is.na(dNSAF_avg.y))
-names(data)[6] <- "dNSAF_maize1"
-names(data)[7] <- "dNSAF_maize2"
 
-data$foldChange_expr <- log2(data$FPKM_maize1) - log2(data$FPKM_maize2)
-data$foldChange_abun <- log2(data$dNSAF_maize1) - log2(data$dNSAF_maize2)
-
-ggplot(data, aes(foldChange_expr, foldChange_abun)) +
-  geom_point(alpha=.1) +
-  xlim(-10,10) + ylim(-10,10) +
-  geom_smooth(method="lm", se=TRUE, fullrange=FALSE, level=0.95) +
-  # facet_wrap(~Sample, nrow=6) +
-  labs(
-    title = paste0("Scatterplot of Expression in Maize1 vs. Abundance in Maize1\nLog-Log Scale for gene pair(s).\nAlpha=.1, linear abline, loess smoothing"),
-    x = "Expression for Maize1 Gene (log scale)",
-    y = "Abundance for Maize1 Gene (log scale)"
-  ) +
-  annotate("text",x=8,y=-8, label=paste("Pearson Cor = ", round(cor(data$foldChange_expr, data$foldChange_abun, method = c("pearson")),2)))
+#--------------------------------------------------------------------------------------------------#
+# Interactive Exploration
+#--------------------------------------------------------------------------------------------------#
+library(rggobi)
+g <- ggobi(data)
 
 #--------------------------------------------------------------------------------------------------#
 detach("package:fitdistrplus", unload=TRUE)
