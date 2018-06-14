b92e590dd309aab60aad6178fa2562ae7e6424eb
Jesse Walsh <jrwalsh@iastate.edu>
2018-06-14 17:17:49 -0500

    User friendly function outputs table of overlap
    Produces table for overlap of reactions and pathway annotations for gene pairs in input. Computed over the whole set, not comparing individual pairs.

--- R/pathway_analysis.R
+++ R/pathway_analysis.R
@@ -1,46 +1,73 @@
 #--------------------------------------------------------------------------------------------------#
 # Pathways
 #--------------------------------------------------------------------------------------------------#
-# ## Load
-corncyc.geneids.clean <- MaizeMap::corncyc.gene.map
-corncyc.reactions.clean <- MaizeMap::corncyc.reaction.gene.map
-corncyc.pathways.clean <- MaizeMap::corncyc.pathway.reaction.map
+getPathwayReactionOverlap <- function(homeologs.pairs) {
+  # ## Load
+  corncyc.geneids.clean <- MaizeMap::corncyc.gene.map
+  corncyc.reactions.clean <- MaizeMap::corncyc.reaction.gene.map
+  corncyc.pathways.clean <- MaizeMap::corncyc.pathway.reaction.map
 
-# ## Clean
-# Merge the pathway/reaction sets
-pathway.reaction.genes <-
-  corncyc.pathways.clean %>%
-  inner_join(corncyc.reactions.clean, by=c("ReactionID"="ReactionID"))
+  # ## Clean
+  # Merge the pathway/reaction sets
+  pathway.reaction.genes <-
+    corncyc.pathways.clean %>%
+    inner_join(corncyc.reactions.clean, by=c("ReactionID"="ReactionID"))
 
-## Do
-# A dataset with homeologous pairs with their frameID's (where available)
-data <-
-  homeologs.pairs %>%
-  subset(Maize1 != "" & Maize2 != "") %>% #comment this line to get an all subgenome comparison instead of only those with homeologs
-  select(Maize1, Maize2) %>%
-  distinct() %>%
-  left_join(corncyc.geneids.clean, by=c("Maize1"="v4_id")) %>%
-  left_join(corncyc.geneids.clean, by=c("Maize2"="v4_id")) %>%
-  rename(FrameID1=FrameID.x, FrameID2=FrameID.y)
+  ## Do
+  # A dataset with homeologous pairs with their frameID's (where available)
+  data <-
+    homeologs.pairs %>%
+    subset(Maize1 != "" & Maize2 != "") %>% #comment this line to get an all subgenome comparison instead of only those with homeologs
+    select(Maize1, Maize2) %>%
+    distinct() %>%
+    left_join(corncyc.geneids.clean, by=c("Maize1"="v4_id")) %>%
+    left_join(corncyc.geneids.clean, by=c("Maize2"="v4_id")) %>%
+    rename(FrameID1=FrameID.x, FrameID2=FrameID.y)
 
-# Reactions associated with Maize1 genes
-df1 <-
-  data %>%
-  subset(!is.na(FrameID1)) %>%
-  left_join(pathway.reaction.genes, by=c("FrameID1"="GeneID")) %>%
-  subset(!is.na(ReactionID)) %>%
-  select(ReactionID) %>%
-  distinct()
+  # Reactions associated with Maize1 genes
+  df1 <-
+    data %>%
+    subset(!is.na(FrameID1)) %>%
+    left_join(pathway.reaction.genes, by=c("FrameID1"="GeneID")) %>%
+    subset(!is.na(ReactionID)) %>%
+    select(ReactionID) %>%
+    distinct()
 
-# Reactions associated with Maize2 genes
-df2 <-
-  data %>%
-  subset(!is.na(FrameID2)) %>%
-  left_join(pathway.reaction.genes, by=c("FrameID2"="GeneID")) %>%
-  subset(!is.na(ReactionID)) %>%
-  select(ReactionID) %>%
-  distinct()
+  # Reactions associated with Maize2 genes
+  df2 <-
+    data %>%
+    subset(!is.na(FrameID2)) %>%
+    left_join(pathway.reaction.genes, by=c("FrameID2"="GeneID")) %>%
+    subset(!is.na(ReactionID)) %>%
+    select(ReactionID) %>%
+    distinct()
 
-nrow(intersect(df1, df2))
-nrow(setdiff(df1, df2))
-nrow(setdiff(df2, df1))
+  nrow(intersect(df1, df2)) #Shared reactions
+  nrow(setdiff(df1, df2)) #Unique to M1
+  nrow(setdiff(df2, df1)) #Unique to M2
+
+
+  # Pathways associated with Maize1 genes
+  df3 <-
+    data %>%
+    subset(!is.na(FrameID1)) %>%
+    left_join(pathway.reaction.genes, by=c("FrameID1"="GeneID")) %>%
+    subset(!is.na(PathwayID)) %>%
+    select(PathwayID) %>%
+    distinct()
+
+  # Pathways associated with Maize2 genes
+  df4 <-
+    data %>%
+    subset(!is.na(FrameID2)) %>%
+    left_join(pathway.reaction.genes, by=c("FrameID2"="GeneID")) %>%
+    subset(!is.na(PathwayID)) %>%
+    select(PathwayID) %>%
+    distinct()
+
+  descriptions <- c("Shared reactions", "Reactions unique to M1", "Reactions unique to M2", "Shared pathways", "Pathways unique to M1", "Pathways unique to M2")
+  values <- c(nrow(intersect(df1, df2)), nrow(setdiff(df1, df2)), nrow(setdiff(df2, df1)), nrow(intersect(df3, df4)), nrow(setdiff(df3, df4)), nrow(setdiff(df4, df3)))
+  out <- data.frame(Description=descriptions, Value=values)
+
+  return(out)
+}
